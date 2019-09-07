1132f07ebf145ac3d689cea55b3737dae37b864b
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-09-07 13:01:43 -0700

    Define instance: no transition is a valid transition

--- src/Control/ValidTransition.purs
+++ src/Control/ValidTransition.purs
@@ -3,3 +3,5 @@ module Control.ValidTransition where
 -- | A type class for defining a valid state transition in
 -- | `IxFunctor`, `IxApply`, `IxApplicative`, `IxBind`, and `IxMonad`
 class ValidTransition x y
+
+instance validTransitionSelfToSelf :: ValidTransition self self
