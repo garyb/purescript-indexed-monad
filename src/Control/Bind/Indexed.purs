7d48b2d8d32a74fe511d7629353ce26b491c7500
Gary Burgess <gary.burgess@gmail.com>
2021-03-05 01:35:24 +0000

    Bump dependencies and add kind signatures

--- src/Control/Bind/Indexed.purs
+++ src/Control/Bind/Indexed.purs
@@ -12,6 +12,7 @@ module Control.Bind.Indexed
 import Prelude (flip, identity, Unit)
 import Control.Apply.Indexed
 
+class IxBind ∷ ∀ ix. (ix → ix → Type → Type) → Constraint
 class IxApply m ⇐ IxBind m where
   ibind ∷ ∀ a b x y z. m x y a → (a → m y z b) → m x z b
 
--- src/Control/Monad/Indexed.purs
+++ src/Control/Monad/Indexed.purs
@@ -16,6 +16,7 @@ import Control.Apply.Indexed (class IxApply, iapply, iapplyFirst, iapplySecond, 
 import Control.Bind.Indexed (class IxBind, ibind, (:>>=), ibindFlipped, (=<<:), composeiKleisli, (:>=>), composeiKleisliFlipped, (<=<:))
 import Data.Functor.Indexed (class IxFunctor, imap, ivoid, ivoidLeft, ivoidRight, (:$>), (<$:))
 
+class IxMonad ∷ ∀ ix. (ix → ix → Type → Type) → Constraint
 class (IxApplicative m, IxBind m) ⇐ IxMonad m
 
 iap ∷ ∀ m a b x y z. IxMonad m ⇒ m x y (a → b) → m y z a → m x z b
